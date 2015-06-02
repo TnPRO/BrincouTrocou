35cb07ee89cb1f4ea529926fb49faeb7f955af9a
Saro Nasr <admin@findmixtapes.com>
2015-06-02 02:47:41 +0000

    Changes made by Saro Nasr (admin@findmixtapes.com).

--- BrincouTrocou/js/log.js
+++ BrincouTrocou/js/log.js
@@ -1,10 +1,9 @@
 function facebook() {
 
-document.getElementById( 'carregando' ).style.display = 'block';
+//document.getElementById( 'carregando' ).style.display = 'block';
 
-    btnLoginClicked();
-/*
-facebookConnectPlugin.login(["email"], function(response)
+
+ facebookConnectPlugin.login(["email"], function(response) 
         {
            
 
@@ -17,44 +16,24 @@ facebookConnectPlugin.login(["email"], function(response)
                          document.getElementById( 'carregando' ).style.display = 'none';
                     }
         });
- */
 }
 
-//login function
-function btnLoginClicked()
-{
-    console.log("login btn clicked");
-    loginUsingFB("arguments");
-}
-
-/*
-function lgnErrHandler(e)
-{
-    alert("unable to login : "+e);
-}
-
-function lgnSuccessHandler(e)
-{
-    alert("logged in - userID : "+e.userInfo.id+" username : "+e.userInfo.name+ " access token "+e.accessToken);
-}
- */
-
-        function processFacebook()
+        function processFacebook() 
         {
                
-		facebookConnectPlugin.api( "me/?fields=id,email,name,birthday", ["user_birthday", "user_friends"],
+		facebookConnectPlugin.api( "me/?fields=id,email,name", ["email"],
 
-                    function (response) { facecheck(JSON.stringify(response.email),JSON.stringify(response.id),JSON.stringify(response.name),JSON.stringify(response.birthday)) },
+                    function (response) { facecheck(JSON.stringify(response.email),JSON.stringify(response.id),JSON.stringify(response.name)) },
 
                     function (response) { alert(JSON.stringify(response)) });
         } 
 
 
+function facecheck(mail,id,name){
 
-function facecheck(mail,id,name,birth){
 
 
-ur = "http://www.brincoutrocou.com.br/check_user.asp?face=T&email=" + mail + "&senha=" + id + "&nome=" + name + "&birth=" + birth;
+ur = "http://www.brincoutrocou.com.br/check_user.asp?face=T&email=" + mail + "&senha=" + id + "&nome=" + name + "&birth=";
 
 
 
@@ -206,11 +185,10 @@ document.getElementById( 'carregando' ).style.display = 'none';
 
 
 
-function checa()
-{
+function checa(){
+
 //alert(document.url);
-    if (window.localStorage.brincoutrocou)
-    {
-        location.href = "trocar.html";
-    }
+if (window.localStorage.brincoutrocou){
+location.href = "trocar.html";
+}
 }
